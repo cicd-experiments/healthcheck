6add475ed50af8b9f2b19491fd2c3f32df012103
Sergey Kovalev <dipdeepcode@mail.ru>
2025-04-18 19:56:53 +0300

    init commit

---
+++ src/main/java/ru/ddc/healthcheck/HealthcheckApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ddc/healthcheck/HealthcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
